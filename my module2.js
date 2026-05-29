c782430fbe4ef5a34a1ff0ad1bfcf8f00211b1ed
Junaid123-malik <Junaidimtiaz928@gmail.com>
2026-05-29 23:17:06 +0500

    Initial commit

---
+++ my module2.js
@@ -0,0 +1,8 @@
+
+
+module.exports={
+    a:1,
+    b:4
+}
+// let c=66
+// module.exports=c
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+import {a,b,c,d} from "./mymodule.js"
+console.log(a,c,d);
+import {juni} from "./mymodule.js"
+console.log(juni);
